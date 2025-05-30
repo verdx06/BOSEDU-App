80d84c6540bb2f2a5a1fa95c819334d680a8aab3
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-05-30 16:08:29 +0300

    onboarding screen add

--- BOSEDU/BOSEDU/Presentation/View/Onboarding/OnboardingView.swift
+++ BOSEDU/BOSEDU/Presentation/View/Onboarding/OnboardingView.swift
@@ -6,11 +6,32 @@
 //
 
 import SwiftUI
+import UIComponents
 
 struct OnboardingView: View {
+    
+    @StateObject var ovm = OnboardingViewModel()
+    
     var body: some View {
         ZStack {
             Color.white.ignoresSafeArea()
+            VStack{
+                HStack {
+                    Image(Images.logoBlue)
+                    Text(Constants.title)
+                        .robotoFont(size: Constants.title_size, font: .extrabold)
+                        .foregroundStyle(Colors.primary80)
+                }
+            Text(Constants.subtitle)
+                .robotoFont(size: Constants.subtitle_size)
+                .foregroundStyle(Colors.primary100)
+                .tracking(6)
+                
+                Spacer()
+                
+                
+        }
+            
         }
     }
 }
---
+++ BOSEDU/BOSEDU/Presentation/ViewModel/OnboardingViewModel/OnboardingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingViewModel.swift
+//  BOSEDU
+//
+//  Created by Виталий Багаутдинов on 30.05.2025.
+//
+
+import Foundation
