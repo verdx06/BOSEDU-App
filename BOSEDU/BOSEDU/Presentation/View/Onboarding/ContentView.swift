d90b1e3aede345c72082f561124d1beefc2ace97
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-05-30 11:56:58 +0300

    create priject

---
+++ BOSEDU/BOSEDU/Presentation/View/Onboarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BOSEDU
+//
+//  Created by Виталий Багаутдинов on 30.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
