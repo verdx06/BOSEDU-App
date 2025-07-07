e1746ec56582538be61142b3185b00bde2cd0959
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-07-07 10:39:50 +0700

    added new models

--- BOSEDU/NetworkService/NetworkService/Models/LoginResponseModel.swift
+++ BOSEDU/NetworkService/NetworkService/Models/LoginResponseModel.swift
@@ -12,10 +12,10 @@ import Foundation
 //"token_type": "string"
 
 public struct TokenResponseModel: Decodable {
-    let accessToken: String
-    let tokenType: String
+    public let accessToken: String
+    public let tokenType: String
     
-    enum CodingKeys: String, CodingKey {
+    public enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
         case tokenType = "token_type"
     }
--- BOSEDU/NetworkService/NetworkService/Models/ServerErrorResponse.swift
+++ BOSEDU/NetworkService/NetworkService/Models/ServerErrorResponse.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+public struct ServerErrorResponse: Decodable {
+    public let detail: String
+    
+    public init(detail: String) {
+        self.detail = detail
+    }
+}
+
+public enum NetworkError: Error, LocalizedError {
+    case invalidURL
+    case noResponse
+    case decodingError(String)
+    case serverError(message: ServerErrorResponse)
+    case unknownError(String)
+    
+    public var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Неверный URL адрес"
+        case .noResponse:
+            return "Нет ответа от сервера"
+        case .decodingError(let message):
+            return "Ошибка обработки данных: \(message)"
+        case .serverError(let message):
+            return message.detail
+        case .unknownError(let message):
+            return "Неизвестная ошибка: \(message)"
+        }
+    }
+}
