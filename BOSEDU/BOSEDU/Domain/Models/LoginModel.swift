139711da461c15747fbf5ee6634c767c7f41753a
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-07-07 10:43:41 +0700

    added new model

--- BOSEDU/BOSEDU/Domain/Models/LoginModel.swift
+++ BOSEDU/BOSEDU/Domain/Models/LoginModel.swift
@@ -5,14 +5,21 @@
 //  Created by Виталий Багаутдинов on 03.07.2025.
 //
 
-import SwiftUI
+import Foundation
+import NetworkService
 
-struct LoginModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LoginModel: Decodable {
+    let accessToken: String
+    let tokenType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
     }
 }
 
-#Preview {
-    LoginModel()
+extension LoginModel {
+    static func convert(from decoder: TokenResponseModel) -> LoginModel {
+        return LoginModel(accessToken: decoder.accessToken, tokenType: decoder.tokenType)
+    }
 }
