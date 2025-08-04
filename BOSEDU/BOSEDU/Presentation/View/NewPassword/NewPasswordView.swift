6ed29b4e1b696172ed59bf25252bdad5662db7d2
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 14:09:13 +0700

    add view for create new password

--- BOSEDU/BOSEDU/Presentation/View/NewPassword/NewPasswordView.swift
+++ BOSEDU/BOSEDU/Presentation/View/NewPassword/NewPasswordView.swift
@@ -6,13 +6,48 @@
 //
 
 import SwiftUI
+import PopupView
+import UIComponents
 
 struct NewPasswordView: View {
+    
+    @StateObject var vm = NewPasswordViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            VStack {
+                
+                Spacer()
+                
+                CustomTextFieldView(titleKey: "New password", security: true, errorText: vm.passwordError, text: $vm.password)
+                CustomTextFieldView(titleKey: "Re-enter password", security: true, errorText: vm.passwordConfirmError, text: $vm.passwordConfirm).padding(.top, geo.size.height * 0.012)
+                
+                PrimaryButtonView(title: "Update password", style: .auth) {
+                    vm.isNavigate = true
+                    vm.isSuccess = true
+                }.padding(.top, geo.size.height * 0.012)
+                
+                Spacer()
+                Spacer()
+                
+            }.navigationBarBackButtonHidden()
+            .headerAuth(title: "New password", dismiss: true)
+            .padding(.horizontal, 20)
+            .alert("Ошибка", isPresented: $vm.isShowAlert) {
+                Button("Ok") {
+                    //
+                }
+            }
+            .navigationDestination(isPresented: $vm.isNavigate) {
+                LoginView(lvm: LoginDI.make())
+                    .successPopUp(isPresented: $vm.isSuccess)
+            }
+        }
     }
 }
 
 #Preview {
-    NewPasswordView()
+    NavigationStack {
+        NewPasswordView()
+    }
 }
