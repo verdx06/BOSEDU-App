bbd2a7f883b88e147cf90f058f1bb881193d1277
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 14:06:28 +0700

    update logic for onboard

--- BOSEDU/BOSEDU/Domain/UseCase/SplashScreenUseCase.swift
+++ BOSEDU/BOSEDU/Domain/UseCase/SplashScreenUseCase.swift
@@ -9,12 +9,23 @@ import Foundation
 
 protocol SplashScreenUseCase {
     func startSplashTimer() async throws
+    func isOnboardingShown() async throws -> Bool
 }
 
 final class SplashScreenUseCaseImpl: SplashScreenUseCase {
     
+    let repository: SplashRepository
+    
+    init(repository: SplashRepository) {
+        self.repository = repository
+    }
+    
     func startSplashTimer() async throws {
         try await Task.sleep(for: .seconds(2))
+    }
+    
+    func isOnboardingShown() async throws -> Bool {
+        return try await repository.isOnboardingShown()
     }
     
 }
