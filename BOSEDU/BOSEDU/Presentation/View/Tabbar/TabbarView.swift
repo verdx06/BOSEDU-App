8b173523caa0be2fd34dc44d80778ee2e271be26
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 14:09:47 +0700

    add check networkconnect

--- BOSEDU/BOSEDU/Presentation/View/Tabbar/TabbarView.swift
+++ BOSEDU/BOSEDU/Presentation/View/Tabbar/TabbarView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct TabbarView: View {
+    
+    @EnvironmentObject private var networkMonitor: NetworkMonitor
+    
     var body: some View {
         ZStack {
             HomeView()
-        }
+        }.networkConnect()
     }
 }
 
 #Preview {
     TabbarView()
+        .environmentObject(NetworkMonitor())
 }
