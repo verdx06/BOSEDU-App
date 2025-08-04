a2aa92e70e85d8b3e25ba388604ea979746842eb
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 13:50:00 +0700

    add check network connect

--- BOSEDU/BOSEDU/Data/Infrastructure/NetworkMonitor.swift
+++ BOSEDU/BOSEDU/Data/Infrastructure/NetworkMonitor.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Network
+
+class NetworkMonitor: ObservableObject {
+    static let shared = NetworkMonitor()
+    
+    private let monitor: NWPathMonitor
+    private let queue = DispatchQueue(label: "NetworkMonitorQueue")
+    
+    @Published var isConnected: Bool = true
+    
+    init() {
+        monitor = NWPathMonitor()
+        startMonitoring()
+    }
+    
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+}
