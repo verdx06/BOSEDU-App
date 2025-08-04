75e2736d56e27317ee7cfd671b077302cccf7d15
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 14:12:12 +0700

    add logic for newpassword

--- BOSEDU/BOSEDU/Presentation/ViewModel/NewPassword/NewPasswordViewModel.swift
+++ BOSEDU/BOSEDU/Presentation/ViewModel/NewPassword/NewPasswordViewModel.swift
@@ -5,14 +5,40 @@
 //  Created by Виталий Багаутдинов on 24.07.2025.
 //
 
-import SwiftUI
+import Foundation
 
-struct NewPasswordViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+final class NewPasswordViewModel: ObservableObject {
+    
+    @Published var password: String = ""
+    @Published var passwordConfirm: String = ""
+    
+    @Published var passwordError: String = ""
+    @Published var passwordConfirmError: String = ""
+    
+    @Published var status: LoadingStatus = .idle
+    @Published var isShowAlert: Bool = false
+    @Published var isNavigate: Bool = false
+    @Published var isSuccess: Bool = false
+    
+    let validationUseCase: ValidationUseCase
+    
+    init(validationUseCase: ValidationUseCase = BaseValidationUseCase()) {
+        self.validationUseCase = validationUseCase
     }
-}
-
-#Preview {
-    NewPasswordViewModel()
+    
+    func updatePassword() {
+        status = .loading
+        passwordError = validationUseCase.validatePassword(password)
+        passwordConfirmError = validationUseCase.validatePasswordConfirmation(password: password, passwordConfirm: passwordConfirm)
+        guard passwordError.isEmpty && passwordConfirmError.isEmpty else {
+            status = .failure("Ошибка")
+            isShowAlert = true
+            return
+        }
+            status = .success
+            print("update Password")
+        isSuccess = isNavigate
+        
+    }
+    
 }
