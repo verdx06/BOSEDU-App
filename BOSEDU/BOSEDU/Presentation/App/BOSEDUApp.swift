4b90a585b0a8bb570360e9e758283ffec9c7238e
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 14:08:43 +0700

    add networkconnect

--- BOSEDU/BOSEDU/Presentation/App/BOSEDUApp.swift
+++ BOSEDU/BOSEDU/Presentation/App/BOSEDUApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct BOSEDUApp: App {
+    
+    @StateObject var networkConnection = NetworkMonitor()
+    
     var body: some Scene {
         WindowGroup {
             SplashScreenView(svm: SplashScreenDI.make())
+                .environmentObject(networkConnection)
         }
     }
 }
