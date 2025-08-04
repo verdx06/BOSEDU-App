42a4cf5731a5633bcf693c85b650d98a9d759f1c
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-04 14:08:02 +0700

    add logic for forgotpassword

--- BOSEDU/BOSEDU/Domain/UseCase/ForgotPasswordUseCase.swift
+++ BOSEDU/BOSEDU/Domain/UseCase/ForgotPasswordUseCase.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+protocol ForgotPasswordUseCase {
+    func sendCode(email: String) async throws
+}
+
+final class ForgotPasswordUseCaseImpl: ForgotPasswordUseCase {
+    
+    let repository: AuthRepository
+    
+    init(repository: AuthRepository) {
+        self.repository = repository
+    }
+    
+    func sendCode(email: String) async throws {
+        try await repository.sendEmailCode(email: email, code: generateCode())
+    }
+    
+    private func generateCode() -> String {
+        let letters = Array("0123456789")
+        var code = ""
+        for _ in 0..<4 {
+            let randomIndex = Int.random(in: 0..<letters.count)
+            code.append(letters[randomIndex])
+        }
+        
+        return code
+        
+    }
+    
+    
+}
